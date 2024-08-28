0aa45da4e4711a83d734cf3e51e17f80a154cf7b
sondos-saad <so_saad@yahoo.com>
2024-08-28 22:50:16 +0300

    router

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,8 @@
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./Components/NavBar/Navbar"
+import Home from "./Pages/Home/Home"
+import Cart from "./Pages/Cart/Cart"
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder"
 
 
 function App() {
@@ -8,6 +12,11 @@ function App() {
     <>
       <div>
             <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/order" element={<PlaceOrder />} />
+            </Routes>
       </div>
     </>
   )
